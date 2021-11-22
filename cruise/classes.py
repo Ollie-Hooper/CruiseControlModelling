ed22f246478553d5ba153d0e5ce1d299319088e2
marjoriewatts <xr20455@bristol.ac.uk>
2021-11-22 15:41:08 +0000

    merging classes fro the car and the torque curve plotting as one

--- cruise/classes.py
+++ cruise/classes.py
@@ -1,4 +1,5 @@
 from math import cos, sin, pi
+import numpy as np
 
 
 class Car:
@@ -80,3 +81,17 @@ class Car:
         d = self.drag(v)
         m = self.mass
         return (A - F - W - d) / m
+
+
+class TorquePlotting:
+
+    def __init__(self, power, torque):
+        self.power = power
+        self.torque = torque
+
+    def torque_curves(self, omega):
+        t = self.torque  # engine torque constant (max torque)
+        omega_m = self.power / t  # max angular velocity
+        beta = 0.4  # max engine rolloff (because you cant move with a torque below a certain value)
+
+        return np.clip(t * (1 - beta * (omega / omega_m - 1) ** 2), 0, None)
